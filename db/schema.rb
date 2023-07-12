f60688961acdda0c4aaedcca0a0b22ee4e0f05cc
ryusi11 <rstoyo.yuji.maeda@gmail.com>
2023-07-12 18:14:56 +0900

    Revert "deviseのインストール"
    This reverts commit 48f00ebac91a566f7ea27d95023e218cd0c9863b.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_090706) do
-  create_table "rooms", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_064135) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
